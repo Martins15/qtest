b861ba7bd1a9dce30d513288b3ea216e6512c357
Martin Malovanets <martin.malovanets@gmail.com>
2022-12-06 19:49:51 +0200

    remove score column from index

--- playground_demo_users_index.sql
+++ playground_demo_users_index.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Dec 05, 2022 at 08:09 PM
+-- Generation Time: Dec 06, 2022 at 05:41 PM
 -- Server version: 10.3.32-MariaDB-1:10.3.32+maria~focal-log
 -- PHP Version: 7.4.27
 
@@ -30,35 +30,34 @@ SET time_zone = "+00:00";
 CREATE TABLE `playground.demo_users_index` (
   `uid` int(10) UNSIGNED NOT NULL DEFAULT 0,
   `field_name` varchar(60) NOT NULL DEFAULT '',
-  `token` varchar(64) DEFAULT '',
-  `score` tinyint(4) NOT NULL
+  `token` varchar(64) DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `playground.demo_users_index`
 --
 
-INSERT INTO `playground.demo_users_index` (`uid`, `field_name`, `token`, `score`) VALUES
-(1, 'mail', 'geoge', 2),
-(1, 'mail', 'schmt', 2),
-(1, 'fullname', 'george', 4),
-(1, 'fullname', 'schmitt', 4),
-(1, 'username', 'additi', 1),
-(1, 'username', 'nte', 1),
-(1, 'username', 'mite', 1),
-(2, 'mail', 'alfnso', 2),
-(2, 'mail', 'hoffan', 2),
-(2, 'fullname', 'alfonso', 4),
-(2, 'fullname', 'hoffman', 4),
-(2, 'username', 'wurfi', 1),
-(2, 'username', 'gd', 1),
-(2, 'username', 'bersome', 1),
-(3, 'fullname', 'quinton', 4),
-(3, 'fullname', 'stevenson', 4),
-(3, 'mail', 'quinon', 2),
-(3, 'mail', 'sevenson', 2),
-(3, 'username', 'mostaf', 1),
-(3, 'username', 'wing', 1);
+INSERT INTO `playground.demo_users_index` (`uid`, `field_name`, `token`) VALUES
+(1, 'mail', 'geoge'),
+(1, 'mail', 'schmt'),
+(1, 'fullname', 'george'),
+(1, 'fullname', 'schmitt'),
+(1, 'username', 'additi'),
+(1, 'username', 'nte'),
+(1, 'username', 'mite'),
+(2, 'mail', 'alfnso'),
+(2, 'mail', 'hoffan'),
+(2, 'fullname', 'alfonso'),
+(2, 'fullname', 'hoffman'),
+(2, 'username', 'wurfi'),
+(2, 'username', 'gd'),
+(2, 'username', 'bersome'),
+(3, 'fullname', 'quinton'),
+(3, 'fullname', 'stevenson'),
+(3, 'mail', 'quinon'),
+(3, 'mail', 'sevenson'),
+(3, 'username', 'mostaf'),
+(3, 'username', 'wing');
 
 --
 -- Indexes for dumped tables
